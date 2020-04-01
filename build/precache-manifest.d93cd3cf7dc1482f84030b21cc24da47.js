97ddce4635e7a41dd0e6c2dad1d760a5b5266f93
Guannan <guannanlv@gmail.com>
2020-04-01 13:39:16 -0400

    I added the build folder to git. If front end has a new build, then put it here and add it to git again

---
+++ build/precache-manifest.d93cd3cf7dc1482f84030b21cc24da47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf63c8a455b95afedb9c601ef2da234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a02f5a7d763db5e18cf",
+    "url": "/static/css/main.913513f7.chunk.css"
+  },
+  {
+    "revision": "49bf933ff62d50a60556",
+    "url": "/static/js/2.44ef4cd4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.44ef4cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a02f5a7d763db5e18cf",
+    "url": "/static/js/main.cce823aa.chunk.js"
+  },
+  {
+    "revision": "706120c4ec030b53f639",
+    "url": "/static/js/runtime-main.0a7eea17.js"
+  },
+  {
+    "revision": "d9cb94ce4843185932d258c5a270519a",
+    "url": "/static/media/botanica.d9cb94ce.png"
+  },
+  {
+    "revision": "490036e7c56ff48152e696fa2946cb1c",
+    "url": "/static/media/botanica1.490036e7.png"
+  },
+  {
+    "revision": "acff303d644140ca989c1ae5e01cfed3",
+    "url": "/static/media/land.acff303d.jpg"
+  },
+  {
+    "revision": "00091bc130f7f60ee7dc255d853a7763",
+    "url": "/static/media/paper.00091bc1.jpg"
+  },
+  {
+    "revision": "f07576fef4a68abb90151629635370b7",
+    "url": "/static/media/water.f07576fe.jpg"
+  }
+]);
